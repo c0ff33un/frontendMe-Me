7b2d1b2f16de18571c728c4c0d7bd237df206518
IntentionallyJuan <juaccardenasgom@unal.edu.co>
2019-05-16 15:43:30 -0500

    Adding react-native-paper dependency

--- src/screens/login/containers/Login.js
+++ src/screens/login/containers/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import UserInput from '../components/UserInput';
 import SignUp from '../components/SignUp';
-import Header from '../components/Header'
+import Header from '../components/Header';
 
 class Login extends Component {
   render() {
