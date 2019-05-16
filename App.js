c79b85812a91ef4dbbdc38e5f4e5e0f52eaaa242
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-05-16 15:41:38 -0500

    Logo

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Login from './src/screens/login/containers/Login'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <PaperProvider>
+        <Login />
+      </PaperProvider>
     );
   }
 }
