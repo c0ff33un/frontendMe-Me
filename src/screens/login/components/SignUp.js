34e4c431ecb142ee27f260466b767e6a5ed45142
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-05-17 11:26:26 -0500

    react-native-elements added

--- src/screens/login/components/SignUp.js
+++ src/screens/login/components/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { SocialIcon } from 'react-native-elements'
 
 
 export default class SignUp extends Component {
@@ -8,7 +9,24 @@ export default class SignUp extends Component {
   render(){
     return (
       <View>
-        <Text> textInComponen </Text>
+        <View style = {{alignSelf: 'center', marginTop: 8, marginBottom: 8}}>
+          <Text>Or</Text>
+        </View>
+        <SocialIcon
+          title='Sign In With Facebook'
+          button
+          type='facebook'
+        />
+        <SocialIcon
+          title='Sign In With Google'
+          button
+          type='google-plus-official'
+        />
+        <View style = {{alignSelf: 'flex-end'}}>
+          <Text>
+            Sign Up
+          </Text>
+        </View>
       </View>
     );
   }
