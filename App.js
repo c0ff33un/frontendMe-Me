12e85d40fe32e5dcbcc9cec96e56536ac057ab48
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-05-17 15:49:16 -0500

    SignUp View added

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import Login from './src/screens/login/containers/Login'
+import SignUp from './src/screens/sign_up/containers/SignUp';
 
 export default class App extends React.Component {
   render() {
     return (
-      //<PaperProvider>
-        <Login />
-      //</PaperProvider>
+      <PaperProvider>
+        <SignUp />
+      </PaperProvider>
     );
   }
 }
