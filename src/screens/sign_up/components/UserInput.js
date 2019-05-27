006c9689e00acd5081000ce5633e349bfb64b491
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-05-27 00:58:24 -0500

    Sign Up working

--- src/screens/sign_up/components/UserInput.js
+++ src/screens/sign_up/components/UserInput.js
@@ -5,13 +5,31 @@ import {
 } from 'react-native';
 import { TextInput, Button, Text } from 'react-native-paper';
 import { Provider as PaperProvider } from 'react-native-paper';
+import SignUp from '../containers/SignUp';
 
 const colorTextInput = "#FF6B35";
+
+function signUp(username, pass, age) {
+  fetch('https://meemperrapi.herokuapp.com/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      handler: username,
+      age: age,
+      pass: pass,
+      password_salt: pass 
+    })
+  })
+}
 
 export default class UserInput extends Component {
   state = {
     user: '',
     pass: '',
+    repass: '',
+    age: ''
   };
 
   render() {
@@ -29,15 +47,15 @@ export default class UserInput extends Component {
             onChangeText={user => this.setState({ user })}
           />
           <View style={{marginLeft: 8}}>
-            <Text>Ingresa tu correo electrónico</Text>
+            <Text>Ingresa tu edad</Text>
           </View>
           <TextInput
             mode="outlined"
             style={ {margin: 8} }
-            value={this.state.user}
+            value={this.state.age}
             selectionColor= { colorTextInput }
             underlineColorAndroid = {colorTextInput}
-            onChangeText={user => this.setState({ user })}
+            onChangeText={age => this.setState({ age })}
           />
           <View style={{marginLeft: 8}}>
             <Text>Ingresa tu contraseña</Text>
@@ -45,10 +63,10 @@ export default class UserInput extends Component {
           <TextInput
             mode="outlined"
             style={ {margin: 8} }
-            value={this.state.user}
+            value={this.state.pass}
             selectionColor= { colorTextInput }
             underlineColorAndroid = {colorTextInput}
-            onChangeText={user => this.setState({ user })}
+            onChangeText={pass => this.setState({ pass })}
           />
           <View style={{marginLeft: 8}}>
             <Text>Ingresa nuevamente tu contraseña</Text>
@@ -56,15 +74,26 @@ export default class UserInput extends Component {
           <TextInput
             mode="outlined"
             style={ {margin: 8} }
-            value={this.state.user}
+            value={this.state.repass}
             selectionColor= { colorTextInput }
             underlineColorAndroid = {colorTextInput}
-            onChangeText={user => this.setState({ user })}
+            onChangeText={repass => this.setState({ repass })}
           />
         
-        <Button style={ {margin: 8} } color="#FF6B35" mode="outlined" title="Registrarse" onPress={() => console.log(this)}>
-          Registrarse
-        </Button>
+        <Button style={ {margin: 8} } color="#FF6B35" mode="outlined" title="Registrarse" 
+          onPress = {() => fetch('https://meemperrapi.herokuapp.com/users', {
+                            method: 'POST',
+                            headers: {
+                              'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                              handle: this.state.user,
+                              age: this.state.age,
+                              pass: this.state.pass,
+                              password_salt: this.state.pass 
+                            })
+                          }
+          )}> Registrarse </Button>
       </View>
     )
   }
