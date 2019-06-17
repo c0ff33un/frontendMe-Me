fbed69a4ceb2351b180a1fc0208f62a42bd6f865
Iván Andrés Lemus <ialemusm@unal.edu.co>
2019-06-17 01:59:51 -0500

    Normal commit, nothing weird wink wink

--- src/SUMMARY_2.md
+++ src/SUMMARY_2.md
@@ -1,29 +1,34 @@
 # Sprint 2
 
-### Dependencias instaladas
-
-- react-native-paper
-- react-native-elements
-- react-navigation
-- react-native-datepicker
-- react-native-vector-icons
-- react-navigation-material-bottom-tabs
-- react-redux
-- redux
-- hybrid-crypto-js
-- js-sha256"
-
 ### Reporte de avance
 
-####1. Vistas 
-En este primer sprint se entrega el landing page de la aplicación, el cual corresponde al login.  
-![Login Page](../login.png)
+####1. Validaciones
 
-Además se añadió la vista de registro. 
-![SignUp Page](../sign.png)
+Se realizan las validaciones en el formulario de registro. Las validaciones son mostradas al usuario por medio de mensajes y código de colores.
 
-####2. Árbol de componentes y Mockups
-El árbol de componentes y mockups se pueden encontrar en el repositorio https://github.com/c0ff33un/adjuntos.git
+![Login Page](../imgs\validacion1.jpg)
+![Login Page](../imgs\validacion2.jpg)
 
-####3. Definidas rutas de aplicación
-Las rutas se difinieron en la ruta /src/routing/MainNavigator.js
+####2. Implementación de autenticación
+Se realizó la implementación para el inicio de sesión y el registro del usuario. Los componentes que contienen la implementación son src\screens\login\components\UserInput.js y src\screens\sign_up\components\UserInput.js para inicio de sesión y registro respectivamente.
+
+####3. Consumo de endpoints de back
+#####POST
+Subir imágenes utilizando la dirección /user/memes del API
+
+![Login Page](../imgs\upload_meme.jpg)
+
+#####GET
+Obtener las imagenes subidas por los usuarios y visualizarlas en el feed
+
+![Login Page](../imgs\feed.jpg)
+
+Obtener estadisticas y mostrarlas al usuario
+
+![Login Page](../imgs\stats.jpg)
+
+####4. Implementar Redux para el manejo de sesiones
+Se almacenó el jwt para mantener la sesión del usuario activa, es posible encontrar la implementación de redux en la carpeta src\redux
+
+####5. Protección de rutas
+Se realizó la protección de rutas en el fichero src\MemeApp.js
--- src/redux/reducers/memes.js
+++ src/redux/reducers/memes.js
@@ -1,14 +1,15 @@
-import { 
+import {
   SET_MEME_FILTER,
   SET_MEMES,
   MEME_FILTERS,
   INVALIDATE_MEMES,
   REQUEST_MEMES,
   RECEIVE_MEMES,
-  INCREASE_MEMES_PAGE
-} from '../actionTypes'
+  INCREASE_MEMES_PAGE,
+  RECEIVE_MEMES_ERROR
+} from "../actionTypes";
 
-const initialState = {memeFilter: 'best', } 
+const initialState = { memeFilter: "best" };
 
 function memes_(
   state = {
@@ -17,67 +18,65 @@ function memes_(
     didInvalidate: false,
     page: 1,
     items: []
-  }, 
+  },
   action
 ) {
-  switch(action.type) {
-    case INVALIDATE_MEMES: 
+  switch (action.type) {
+    case INVALIDATE_MEMES:
       return Object.assign({}, state, {
         didInvalidate: true,
         isRefreshing: true,
         page: 1,
         items: []
-      })
+      });
     case REQUEST_MEMES:
       return Object.assign({}, state, {
         isFetching: true,
         didInvalidate: false
-      })
+      });
     case RECEIVE_MEMES:
-      const { memes, receivedAt } = action.payload
+      const { memes, receivedAt } = action.payload;
       return Object.assign({}, state, {
         isFetching: false,
         isRefreshing: false,
         didInvalidate: false,
         items: [...state.items, ...memes],
         lastUpdated: receivedAt
-      })
+      });
     case RECEIVE_MEMES_ERROR:
       return Object.assign({}, state, {
         isFetching: false,
         isRefreshing: false
-      })
+      });
     case INCREASE_MEMES_PAGE:
       return Object.assign({}, state, {
         page: state.page + 1
-      })
+      });
     default:
-      return state
+      return state;
   }
 }
 
 export function memesByFilter(state = {}, action) {
-  switch(action.type) {
+  switch (action.type) {
     case INVALIDATE_MEMES:
     case RECEIVE_MEMES:
     case INCREASE_MEMES_PAGE:
     case REQUEST_MEMES:
-      const { filter } = action.payload
+      const { filter } = action.payload;
       return Object.assign({}, state, {
         [filter]: memes_(state[filter], action)
-      })
+      });
     default:
-      return state
+      return state;
   }
 }
 
-export function selectedFilter(state = 'best', action) {
-  switch(action.type) { 
+export function selectedFilter(state = "best", action) {
+  switch (action.type) {
     case SET_MEME_FILTER:
-      return action.payload.filter
+      return action.payload.filter;
     default:
-      return state
+      return state;
   }
 }
-
-
